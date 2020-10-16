bd460c69b24dc3bfef05610a9116b3350a51e65c
Pierre <pierregiddio@gmail.com>
2020-10-16 16:34:26 +0200

    comments and cleanings

--- src/createCards.js
+++ src/createCards.js
@@ -4,26 +4,22 @@ const createCard = (
     imageUrl = "https://source.unsplash.com/random",
     desc = " "
 ) => {
-    // console.log("hello card")
+    // Our goals is to have function that create such a card
 
     {/* <div class="card">
-        <div class="card-header">
-            <div class="card-header">
+            <div class="card-header"> </div>
+            <div class="card-body">
+                <h3 class="card-title">Modern flat in Milano</h3>
+                <p class="card-description">This descthe perfect flat to chill in Milano for you</p>
             </div>
-        </div>
-        <div class="card-body">
-            <h3 class="card-title">Modern flat in Milano</h3>
-            <p class="card-description">This descthe perfect flat to chill in Milano for you</p>
-        </div>
     </div> */}
 
 
-                    // Card Preparation
+    // Card Preparation
 
     // 1 - The Card
     const card = document.createElement("div")
     card.classList.add("card")
-
 
         // 2 - Add Header to Card
     const cardHeader = document.createElement("div")
@@ -36,20 +32,20 @@ const createCard = (
     cardImg.style.backgroundImage = `url(${imageUrl})`
     cardHeader.appendChild(cardImg)
 
-        // 5 - Create a body card and insert it in the Card
+        // 4 - Create a body card and insert it in the Card
 
     const cardBody = document.createElement("div");
     cardBody.classList.add("card-body");
     card.appendChild(cardBody)
 
-             // 6 - Insert a title in the body
+             // 5 - Insert a title in the body
 
     const cardTitle = document.createElement("h3");
     cardTitle.classList.add("card-title")
     cardTitle.innerHTML = title
     cardBody.appendChild(cardTitle);
 
-             // 7 – Insert a text within the body
+             // 6 – Insert a paragraph within the body
 
     const cardDescription = document.createElement("p");
     cardDescription.classList.add("card-description")
@@ -57,7 +53,8 @@ const createCard = (
     cardBody.appendChild(cardDescription);
 
 
-            // Now, the card is ready, insert the card in the target div
+    // Now, the card is ready, insert the card in the target div
+
     div.appendChild(card)
 }
 
--- src/script.js
+++ src/script.js
@@ -11,21 +11,23 @@ const searchInput = document.querySelector(".search-input")
 const selector = document.querySelector(".select")
 const availableCheckbox = document.querySelector(".available-checkbox");
 
-
 const render = ( housseList ) => {
-    cards.innerHTML = "";
+    // Whatever is the array passed housseList argument, this function
+    // will generate a card for each element of this array
+    cards.innerHTML = ""; // Lets clear the content of cards, in case there is previous results
     housseList.forEach(house => {
         createCards(cards, house.name, house.img, house.desc )
     });
 }
 
-
+// Let's add an event listener on this input
 searchInput.addEventListener("input", (event) => {
-  let filterdListing = houseToRent.filter( house => house.name.includes(event.target.value))
+  // lets filter our list according to new value typed in the input
+  let filterdListing = houseToRent.filter(house => house.name.includes(event.target.value))
+  // and we render again. Remember, within render, we will clear the content, and recreate a card for each
+  // housses of the lsting given as argument.
   render(filterdListing);
 });
-
-
 
 selector.addEventListener("change", (event) => {
   if (event.target.value === "All") {
@@ -36,7 +38,6 @@ selector.addEventListener("change", (event) => {
   }
 });
 
-
 availableCheckbox.addEventListener("click", (event) => {
   if (event.target.checked) {
     let filteredListing = houseToRent.filter(house => house.available === true)
@@ -46,4 +47,5 @@ availableCheckbox.addEventListener("click", (event) => {
   }
 });
 
-render(houseToRent)
+// This function is executed, with the full list of rentings on the first loading of this script
+render(houseToRent);
